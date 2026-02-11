8ff7652e9995a22825f609ec0cdf8f4c3d855502
Your Name <you@example.com>
2026-02-11 21:57:06 +0900

    Fix login method location parameter

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ try:
     )
     
     # Render login widget
-    name, authentication_status, username = authenticator.login('Login', 'main')
+    name, authentication_status, username = authenticator.login(location='sidebar')
     
     if authentication_status == False:
         st.error('Username/password is incorrect')
